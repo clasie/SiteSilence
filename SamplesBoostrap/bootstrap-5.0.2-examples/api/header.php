fc994a1d1c341cfb21f0acd59ae9120b7bf57d35
clasieLinux <claude_siefers@hotmail.com>
2024-11-19 18:58:30 +0100

    ignore

--- SamplesBoostrap/bootstrap-5.0.2-examples/api/header.php
+++ SamplesBoostrap/bootstrap-5.0.2-examples/api/header.php
@@ -5,7 +5,7 @@ header('Content-Type: application/json');
  * 
  */
 //$result = 'Test';
-$host = 'cinqjom24.mysql.db'; //test1
+$host = 'cinqjom24.mysql.db'; //test2
 $dbname = 'cinqjom24';  
 $username = 'cinqjom24'; 
 $password = 'GEOgeo231094';
